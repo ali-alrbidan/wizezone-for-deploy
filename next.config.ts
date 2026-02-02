abed63cacf611c7510ed2b91b38b7b760226ca9f
ali-alrbidan <a30960314@gmail.com>
2026-02-02 15:16:50 -0800

    add files

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    reactStrictMode: true,
   images: {
     domains: ['localhost'], // بدون http:// أو port
+    unoptimized: process.env.NODE_ENV === 'production',
   },
 };
 
