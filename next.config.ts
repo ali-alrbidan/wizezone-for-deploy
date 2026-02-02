af6556e3f7d187b20a76b26c674676bb5dd55b3e
ali-alrbidan <a30960314@gmail.com>
2026-02-02 15:49:44 -0800

    correct next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,41 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  images: {
-    domains: ['localhost'], // بدون http:// أو port
+
+const nextConfig = {
+    reactStrictMode: true,
+      async rewrites() {
+    return [
+      {
+        source: '/api/news/:path*',
+        destination: 'http://147.93.120.97:5000/api/News/:path*',
+      },
+      {
+        source: '/api/:path*',
+        destination: 'http://147.93.120.97:5000/api/:path*',
+      },
+    ];
   },
+  images: {
+  remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // Allow all images for now
+      },
+    ],
+  },
+    // Add trailing slash for better compatibility
+  trailingSlash: false,
+  
+  // Skip type checking during build (if needed)
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+    experimental: {
+    serverActions: {
+      allowedOrigins: ['147.93.120.97:5000'],
+    },
+  },
+  
+
 };
 
-export default nextConfig;
+module.exports = nextConfig
